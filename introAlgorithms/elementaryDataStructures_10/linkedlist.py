6b125faa6a8287435b53ac817f15f8e30e416b5e
Hiep Pham <hieppm.work@gmail.com>
2020-07-12 09:29:55 +0700

    Build node class

--- introAlgorithms/elementaryDataStructures_10/linkedlist.py
+++ introAlgorithms/elementaryDataStructures_10/linkedlist.py
@@ -1,8 +1,72 @@
+class Node():
+    def __init__(self, key, left=None, right=None, parent=None):
+        self.key = key
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+
+    def set_relative(self, id, obj):
+        if id == 1: 
+            self.left = obj
+        elif id == 2:
+            self.right = obj
+        else:
+            self.parent = obj
+
+
+    def rm_relative(self, id):
+        if id == 1: 
+            self.left = None
+        elif id == 2:
+            self.right = None
+        else:
+            self.parent = None    
+
+
+    def has_right(self):
+        if self.right is not None:
+            return True
+        else: 
+            return False
+
+
+    def has_left(self):
+        if self.left is not None:
+            return True
+        else: 
+            return False
+
+    def has_parent(self):
+        if self.parent is not None:
+            return True
+        else: 
+            return False        
+
+
+    def get_key(self):
+        return self.key
+
+
+    def get_right(self):
+        return self.right
+
+
+    def get_left(self):
+        return self.left
+
+
+    def get_parent(self):
+        return self.parent
+
+
+
 class DoublyList:
-	pass
+    def __init__(self): pass
+
+    def next(self): pass
 
 
-class SinglyList:
-	pass
+class SinglyList: pass
 
 
